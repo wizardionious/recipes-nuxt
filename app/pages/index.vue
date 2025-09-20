16a218acbfd61c3c264b07b41de28e2fa09492f9
Andrii <wizardionious.education@gmail.com>
2025-09-20 22:02:41 +0300

    index page

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+useHead({
+    titleTemplate(title) {
+        return `${title} | Home`;
+    },
+});
+</script>
+
+<template>
+    <main class="flex-1">
+        <span>Index Page</span>
+    </main>
+</template>
