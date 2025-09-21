67c338a491f71e0f60230201f12251d2d1d26580
Andrii <wizardionious.education@gmail.com>
2025-09-21 19:13:47 +0300

    dish types

--- shared/types/dish.d.ts
+++ shared/types/dish.d.ts
@@ -1,7 +1,28 @@
+export type Macronutrient = {
+    displayName: string;
+    amount: number;
+};
+
+export type DishIngridient = {
+    title: string;
+} & (
+    | {
+          amount: number;
+          units: string;
+      }
+    | {
+          amount: string;
+      }
+);
+
 export type AppDish = {
+    id: string;
     image: {
         alt: string;
         src: string;
     };
     title: string;
+    description: string;
+    macronutrients: Macronutrient[];
+    ingridients: DishIngridient[];
 };
