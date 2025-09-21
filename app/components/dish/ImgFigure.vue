64ba24897bd5f173fd77f2b27693655a4156fccf
Andrii <wizardionious.education@gmail.com>
2025-09-21 21:41:48 +0300

    img figure

---
+++ app/components/dish/ImgFigure.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+    dish: AppDish;
+}>();
+</script>
+
+<template>
+    <figure>
+        <img
+            :src="dish.image.src"
+            :alt="dish.image.alt"
+            class="w-full rounded-lg object-contain"
+        />
+        <figcaption class="sr-only">{{ dish.dislayName }}</figcaption>
+    </figure>
+</template>
