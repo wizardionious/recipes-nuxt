0124b5ca8df38acfda5c91bdf6c270fcf3b1438c
Andrii <wizardionious.education@gmail.com>
2025-09-21 21:41:34 +0300

    use store

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,78 +5,7 @@ useHead({
     },
 });
 
-const dishes = reactive<AppDish[]>([
-    {
-        id: crypto.randomUUID(),
-        image: {
-            alt: "Чахохбілі",
-            src: "/images/chakhokhbili.png",
-        },
-        title: "Чахохбілі",
-        description:
-            "Классическое грузинское блюдо: тушёная курица с томатами, луком и специями. Высокий белок для мышц, тёплое согревающее блюдо для осени.",
-        macronutrients: [
-            {
-                displayName: "Білки",
-                amount: 48,
-            },
-            {
-                displayName: "Жири",
-                amount: 20,
-            },
-            {
-                displayName: "Вуглеводи",
-                amount: 22,
-            },
-        ],
-        ingridients: [
-            {
-                title: "Курячі стегна без шкіри",
-                amount: 600,
-                units: "г.",
-            },
-            {
-                title: "Цибуля ріпчаста",
-                amount: 300,
-                units: "г.",
-            },
-            {
-                title: "Помідори стиглі (або томати у власному соку)",
-                amount: 500,
-                units: "г.",
-            },
-            {
-                title: "Перець солодкий",
-                amount: 200,
-                units: "г.",
-            },
-            {
-                title: "Часник",
-                amount: 4,
-                units: "зубчики",
-            },
-            {
-                title: "Кінза свіжа",
-                amount: 20,
-                units: "г.",
-            },
-            {
-                title: "Петрушка свіжа",
-                amount: 20,
-                units: "г.",
-            },
-            {
-                title: "Олія оливкова",
-                amount: 200,
-                units: "г.",
-            },
-            {
-                title: "Сіль, перець, хмелі-сунелі",
-                amount: "за смаком",
-            },
-        ],
-    },
-]);
+const dishesStore = useDishesStore();
 </script>
 
 <template>
@@ -89,7 +18,7 @@ const dishes = reactive<AppDish[]>([
 
         <div class="grid flex-1 grid-cols-1 gap-4 p-4">
             <DishCard
-                v-for="dish in dishes"
+                v-for="dish in dishesStore.dishes"
                 :key="dish.id"
                 :dish
             />
