d3be6fa8ad0605250e2db29f58d44952d26bb744
Andrii <wizardionious.education@gmail.com>
2025-09-21 21:42:02 +0300

    link to dish details

--- app/components/dish/Card.vue
+++ app/components/dish/Card.vue
@@ -2,24 +2,23 @@
 const props = defineProps<{
     dish: AppDish;
 }>();
+
+const createLink = (dish: AppDish) => {
+    const escapedTitle = dish.title.replaceAll(" ", "-").toLowerCase();
+
+    return `${escapedTitle}-${dish.id}`;
+};
 </script>
 
 <template>
     <section class="bg-card rounded-lg px-4 shadow-xs">
         <div class="flex items-center justify-center p-3">
             <h2 class="font-caveat text-4xl">
-                {{ dish.title }}
+                {{ dish.dislayName }}
             </h2>
         </div>
 
-        <figure>
-            <img
-                :src="dish.image.src"
-                :alt="dish.image.alt"
-                class="w-full rounded-lg object-contain"
-            />
-            <figcaption class="sr-only">{{ dish.title }}</figcaption>
-        </figure>
+        <DishImgFigure :dish />
 
         <div class="flex py-2">
             <p>{{ dish.description }}</p>
@@ -27,12 +26,21 @@ const props = defineProps<{
 
         <div class="flex gap-1">
             <DishMacros
-                :dish-title="dish.title"
+                :dish-title="dish.dislayName"
                 :macronutrients="dish.macronutrients"
             />
         </div>
 
         <DishIngridients :ingridients="dish.ingridients" />
+
+        <div class="flex items-center justify-center p-4">
+            <NuxtLink
+                :to="createLink(dish)"
+                class="border-border rounded-lg border p-2"
+            >
+                Перейти до приготування
+            </NuxtLink>
+        </div>
 
         <div class="flex gap-2 py-4">
             <button
