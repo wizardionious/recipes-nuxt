63e759b39e47658a30436614b0eba7eda720e86e
Andrii <wizardionious.education@gmail.com>
2025-10-03 17:38:32 +0300

    form field

---
+++ app/components/form/Field.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+const label = defineProps<{
+  label: string;
+}>();
+
+const id = useId();
+provide("input-id", id);
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-2">
+    <label
+      :for="id"
+      class="font-bad-script text-lg"
+    >
+      {{ label }}
+    </label>
+
+    <slot />
+  </div>
+</template>
