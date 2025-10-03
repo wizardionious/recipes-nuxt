84acce2eb5cd74d480314601689432dbffc9a145
Andrii <wizardionious.education@gmail.com>
2025-10-03 17:23:39 +0300

    form textarea

---
+++ app/components/form/Textarea.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+const model = defineModel<string | number>();
+
+const props = withDefaults(
+  defineProps<{
+    name: string;
+    placeholder?: string;
+    required?: boolean;
+    rows?: number;
+    cols?: number;
+  }>(),
+  {
+    placeholder: "Введіть текст",
+    rows: 4,
+    cols: 24,
+  },
+);
+
+const id = inject("input-id") as string;
+</script>
+
+<template>
+  <textarea
+    :id
+    v-bind="props"
+    v-model="model"
+    class="bg-accent-background rounded-lg p-1 px-2"
+  />
+</template>
