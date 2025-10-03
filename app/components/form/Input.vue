a0cc5913b83c2a787e5000ec716a3a3548aeb789
Andrii <wizardionious.education@gmail.com>
2025-10-03 17:28:21 +0300

    form input

---
+++ app/components/form/Input.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+const model = defineModel<string | number>();
+
+const props = withDefaults(
+  defineProps<{
+    type?: InputHTMLAttributes["type"];
+    name: string;
+    placeholder?: string;
+    required?: boolean;
+  }>(),
+  {
+    type: "text",
+    placeholder: "Введіть текст",
+  },
+);
+
+const id = inject("input-id") as string;
+
+onMounted(() => {
+  if (!id || id.trim() === "") {
+    console.error("No id provided for input");
+  }
+});
+</script>
+
+<template>
+  <input
+    :id
+    v-bind="props"
+    v-model="model"
+    class="bg-accent-background rounded-lg p-1 px-2"
+  />
+</template>
