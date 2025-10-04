267f4509e4184d493054d9258db4802b1e248112
Andrii <wizardionious.education@gmail.com>
2025-10-04 17:46:41 +0300

    format

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -3,22 +3,22 @@
  * @type {import("prettier").Config}
  */
 const config = {
-    tabWidth: 2,
-    bracketSameLine: false,
-    useTabs: false,
-    printWidth: 80,
-    semi: true,
-    singleAttributePerLine: true,
-    trailingComma: "all",
-    overrides: [
-        {
-            files: "*.vue",
-            options: {
-                parser: "vue",
-            },
-        },
-    ],
-    plugins: ["prettier-plugin-tailwindcss"],
+  tabWidth: 2,
+  bracketSameLine: false,
+  useTabs: false,
+  printWidth: 80,
+  semi: true,
+  singleAttributePerLine: true,
+  trailingComma: "all",
+  overrides: [
+    {
+      files: "*.vue",
+      options: {
+        parser: "vue",
+      },
+    },
+  ],
+  plugins: ["prettier-plugin-tailwindcss"],
 };
 
 export default config;
