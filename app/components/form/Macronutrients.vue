fc8705daf61ef93a5d58b7390ae5906912162add
Andrii <wizardionious.education@gmail.com>
2025-10-12 18:19:59 +0300

    fix: correct macronutrients v-for loop syntax

---
+++ app/components/form/Macronutrients.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+const model = defineModel<DishMacronutrients>({ required: true });
+</script>
+
+<template>
+  <section class="flex flex-col gap-2">
+    <h2 class="font-bad-script text-lg">Макронутрієнти:</h2>
+
+    <ul class="space-y-2">
+      <FormField
+        v-for="(macro, name) in model"
+        :key="name"
+        :label="macro.displayName"
+        as="li"
+      >
+        <FormInput
+          v-model="model[name].amount"
+          type="number"
+          placeholder="0"
+          :name="name"
+        />
+      </FormField>
+    </ul>
+  </section>
+</template>
