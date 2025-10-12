d224cbd7e87437031866ee18874bf31b62d3ee87
Andrii <wizardionious.education@gmail.com>
2025-10-12 16:59:32 +0300

    deletable row

---
+++ app/components/DeletableRow.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+defineEmits<{
+  delete: [];
+}>();
+
+const props = defineProps<{
+  as: "li" | "div";
+}>();
+</script>
+
+<template>
+  <component
+    :is="as"
+    class="grid grid-cols-8 items-center gap-2"
+  >
+    <slot />
+
+    <button
+      type="button"
+      class="trasnition-colors inline-flex h-full items-center justify-center rounded-lg text-red-500 hover:bg-red-50 hover:text-red-700 active:bg-red-100"
+      title="Видалити інгредієнт"
+      @click="$emit('delete')"
+    >
+     <Icon name="lucide:trash-2" size="20"/>
+    
+    </button>
+  </component>
+</template>
