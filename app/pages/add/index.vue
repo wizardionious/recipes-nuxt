724790ba54020c502aeb1ec3c213ec28582bd098
Andrii <wizardionious.education@gmail.com>
2025-10-12 18:03:17 +0300

    macronutrients

--- app/pages/add/index.vue
+++ app/pages/add/index.vue
@@ -66,8 +66,37 @@ const deleteIngredient = (index: number) => {
             Add ingridient
           </button>
         </section>
-        <section>
+        <section class="flex flex-col gap-2">
           <h2 class="font-bad-script text-lg">Макронутрієнти:</h2>
+          <ul class="space-y-2">
+            <li>
+              <FormField label="Білки">
+                <FormInput
+                  type="number"
+                  placeholder="0"
+                  name="proteins"
+                />
+              </FormField>
+            </li>
+            <li>
+              <FormField label="Жири">
+                <FormInput
+                  type="number"
+                  placeholder="0"
+                  name="fats"
+                />
+              </FormField>
+            </li>
+            <li>
+              <FormField label="Вуглеводи">
+                <FormInput
+                  type="number"
+                  placeholder="0"
+                  name="carbs"
+                />
+              </FormField>
+            </li>
+          </ul>
         </section>
       </Form>
     </div>
