4034efbab2ba6d5ecf3d21430b3984da2bbd4f00
Andrii <wizardionious.education@gmail.com>
2025-10-12 19:18:07 +0300

    styles

--- app/components/PageHeading.vue
+++ app/components/PageHeading.vue
@@ -4,7 +4,7 @@ defineProps<{ text: string }>();
 
 <template>
   <div
-    class="bg-card mx-4 flex items-center justify-center rounded-xl p-2 shadow-xs"
+    class="bg-card flex items-center justify-center rounded-xl p-2 shadow-xs"
   >
     <h1 class="font-caveat text-4xl">{{ text }}</h1>
   </div>
